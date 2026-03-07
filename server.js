291e5b8ddfd8d2cf0e8ba611bd8da9bf07e14e11
ghostkaiba <58136915+ghostkaiba@users.noreply.github.com>
2026-03-07 00:10:11 -0800

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ let serverInitialRerolls = 12; // NEW: Configurable initial re-rolls
 let serverPlayers = []; // { id, name, roles: {}, protectedPickUsed: false, rerollsRemaining: 12 } // Added rerolls
 const maxPlayers = 10;
 const battleRoles = [ "Captain", "Vice Captain", "Ace", "Flex 1/Tactician", "Tank", "Wildcard", "Mascot", "Flex 2/Support" ];
-const relationshipRoles = [ "Primary Partner", "Past Flame", "Guardian Figure", "The Anchor", "Secret Interest", "Further Complication", "Best Friend", "Sibling" ];
+const relationshipRoles = [ "Mommy/Daddy", "Crazy Ex", "Mommy/Daddy", "Baby Mommy/Baby Daddy", "Side Piece", "Other Side Piece", "Best Friend", "Sibling" ];
 const allRoles = [...battleRoles, ...relationshipRoles];
 const totalRoles = allRoles.length;
 
